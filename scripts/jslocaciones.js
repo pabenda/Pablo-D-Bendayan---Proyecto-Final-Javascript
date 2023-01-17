e5fff9eba27b3e02eccc2ba7549c589f0fa9a590
Pablo D. Bendayan <pablodb@gmail.com>
2023-01-17 13:17:37 -0300

    comienzo armado fetch

--- scripts/jslocaciones.js
+++ scripts/jslocaciones.js
@@ -1,3 +1,40 @@
+let showSearchingUsers = document.getElementById("showSearchingUsers");
+let showSearchingPosts = document.getElementById("showSearchingPosts");
+//showSearchingUsers.style.display = "block";
+
+// Sección para consultar Usuarios
+function renderUserDetail(user) {
+  let productDetail = document.getElementById("usersTableBody");
+  let record = document.createElement("tr");
+  record.innerHTML = `<td>${user.name}</td>
+      <td><a href="mailto:${user.email}?Subject=Lo%20contactamos%20para%20ofrecerle%20un%20curso">${user.email}</a></td>
+      <td>${user.username}</td>
+      <td>
+      <a href="${user.website}" target="_blank">${user.website}</a>
+      </td>`;
+  productDetail.appendChild(record);
+}
+
+function renderUsersTableDetails(users = []) {
+  let usersTableBody = document.getElementById("usersTableBody");
+  usersTableBody.innerHTML = "";
+  users.forEach((element) => {
+    renderUserDetail(element);
+  });
+}
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    showSearchingUsers.style.display = "block";
+
+    renderUsersTableDetails(json);
+
+    showSearchingUsers.style.display = "none";
+  });
+
+
 function funcionLocaciones() {
   let x = document.getElementById("localOStand").value;
   let btnLocales = document.getElementById("botonEnviar1");
